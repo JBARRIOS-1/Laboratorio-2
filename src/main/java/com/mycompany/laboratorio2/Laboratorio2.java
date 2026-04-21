f300dc12a9ea28c6500e8cfcc4f4322eb463a408
JBARRIOS-1 <barrioavasquez@gmail.com>
2026-04-20 22:17:00 -0600

    Resubiendo Clase principal y interfaz login
    Elimine por error la informacion anterior, subiendo la informacion recuperada

---
+++ src/main/java/com/mycompany/laboratorio2/Laboratorio2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.laboratorio2;
+
+/**
+ *
+ * @author umg
+ */
+public class Laboratorio2 {
+
+    public static void main(String[] args) {
+        
+    login lg = new login ();
+    lg.setVisible(true); 
+}
+}
