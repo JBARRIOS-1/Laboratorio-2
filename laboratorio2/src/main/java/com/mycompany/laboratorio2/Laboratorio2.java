56221cf66538d3ffdb8466efa2ef271cb9738a60
JBARRIOS-1 <barrioavasquez@gmail.com>
2026-04-24 16:48:59 -0600

    Fix project
     structure and upload full NetBeans Maven project including initial file with one credential for testing porpuses Jvasquez

--- laboratorio2/src/main/java/com/mycompany/laboratorio2/Laboratorio2.java
+++ laboratorio2/src/main/java/com/mycompany/laboratorio2/Laboratorio2.java
@@ -1,4 +1,4 @@
- /*
+    /*
      * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
      */
 
@@ -12,32 +12,21 @@
 
         public static void main(String[] args) {
             
-        Usermaintenance.cargarUsuarios();
-        if (!Usermaintenance.usuarios.contains("Jvasquez")) {
-            
+            Usermaintenance.cargarUsuarios();
+        
+            if(!Usermaintenance.usuarios.contains("Jvasquez")){
+
             Usermaintenance.usuarios.add("Jvasquez");
             Usermaintenance.passwords.add("Umariano24");
             Usermaintenance.estados.add("Active");
             Usermaintenance.roles.add("Admin");
-            Usermaintenance.intentos.add(0);
-
-        } else {
-
+            } else {
             int index = Usermaintenance.usuarios.indexOf("Jvasquez");
-
- 
             Usermaintenance.roles.set(index, "Admin");
             Usermaintenance.estados.set(index, "Active");
-
-    
-            if (Usermaintenance.intentos.size() <= index) {
-                Usermaintenance.intentos.add(0);
-            } else {
-                Usermaintenance.intentos.set(index, 0);
             }
-        }
 
-        
-        new login().setVisible(true);
+            login lg = new login();
+            lg.setVisible(true);
         }
     }
